c21df6caa892d639b457a83398558755dbfe4171
Merijn <merijn@ponzo.nl>
2023-10-25 15:36:27 +0200

    fix nav

--- build/blocks/repeater-nav/index.asset.php
+++ build/blocks/repeater-nav/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e0742c5b7f1aebc7b5a0');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '46f79ca44545ae5d4490');
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'blockbite/blockbite',
-        'pretty_version' => '1.0.37',
-        'version' => '1.0.37.0',
+        'pretty_version' => '1.0.38',
+        'version' => '1.0.38.0',
         'reference' => NULL,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'blockbite/blockbite' => array(
-            'pretty_version' => '1.0.37',
-            'version' => '1.0.37.0',
+            'pretty_version' => '1.0.38',
+            'version' => '1.0.38.0',
             'reference' => NULL,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
